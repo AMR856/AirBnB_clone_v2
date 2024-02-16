713408f938c55cbbe0706ffa3bac10f2cf831990
ameralnas <amer.live477@gmail.com>
2024-02-16 18:53:06 +0200

    Hi dude

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# A script to configure some stuff
+sudo apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "Hi Nginx" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -HR ubuntu:ubuntu /data/
+sudo sed -i '38i#\tlocation / hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
