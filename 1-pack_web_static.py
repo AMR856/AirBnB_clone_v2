ce94b528590fa58ba6efbca57e176c5ebb74d37c
ameralnas <amer.live477@gmail.com>
2024-02-16 19:54:19 +0200

    Go now

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module here to do stuff"""
+from fabric.api import local
+from datetime import datetime
+import os
+def do_pack():
+    """A function to make a tar file fro the folder"""
+    try:
+        if os.path.isdir("versions") is False:
+            local("mkdir versions")
+        theDateVar = datetime.now().strptime("%Y%m%d%H%M%S")
+        theFileName = f"versions/web_static_{theDateVar}"
+        local(f"tar -cvzf {theFileName} web_static")
+        return f"{theFileName}.tgz"
+    except:
+        None
