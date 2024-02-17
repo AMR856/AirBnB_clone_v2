a43a190c0b1976e125adc32ad98eba0314931e45
ameralnas <amer.live477@gmail.com>
2024-02-17 02:48:51 +0200

    Better now

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ import os
 
 env.hosts = ['34.224.3.252', '100.25.129.71']
 
+
 def do_deploy(archive_path):
     """Just be moduled for me"""
     if os.path.isfile(archive_path) is False:
@@ -15,17 +16,23 @@ def do_deploy(archive_path):
 
     if put(archive_path, f"/tmp/{fileNameAll}").failed is True:
         return False
-    if run(f"mkdir -p /data/web_static/releases/{fileNameWithoutExt}/").failed is True:
+    if run(f"mkdir -p /data/web_static/releases/"
+            "{fileNameWithoutExt}/").failed is True:
         return False
-    if run(f"tar -xzf /tmp/{fileNameAll} -C /data/web_static/releases/{fileNameWithoutExt}/").failed is True:
+    if run(f"tar -xzf /tmp/{fileNameAll} -C"
+           "/data/web_static/releases/{fileNameWithoutExt}/").failed is True:
         return False
     if run(f"/tmp/{fileNameAll}").failed is True:
         return False
-    if run(f"mv //data/web_static/releases/{fileNameWithoutExt}/web_static/* /data/web_static/releases/{fileNameWithoutExt}/").failed is True:
+    if run("mv //data/web_static/releases/"
+           f"{fileNameWithoutExt}/web_static/* /data/web_static/releases/"
+           f"{fileNameWithoutExt}/").failed is True:
         return False
-    if run(f"rm -rf /data/web_static/releases/{fileNameWithoutExt}/web_static").failed is True:
+    if run(f"rm -rf /data/web_static/releases/"
+           "{fileNameWithoutExt}/web_static").failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run(f"ln -s /data/web_static/releases/{fileNameWithoutExt} /data/web_static/current").failed is True:
+    if run("ln -s /data/web_static/releases/"
+           f"{fileNameWithoutExt} /data/web_static/current").failed is True:
         return False
