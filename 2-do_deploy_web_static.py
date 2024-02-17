05ed42575fdfbff659a1bcc0e6397fe6c7247fe4
ameralnas <amer.live477@gmail.com>
2024-02-17 03:09:42 +0200

    just work

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,7 +20,7 @@ def do_deploy(archive_path):
         return False
     if run(f"tar -xzf /tmp/{fileA} -C {Path}{fileNo}/").failed is True:
         return False
-    if run(f"/tmp/{fileA}").failed is True:
+    if run(f"rm /tmp/{fileA}").failed is True:
         return False
     if run(f"mv {Path}{fileNo}/web_static/* {Path}{fileNo}/").failed is True:
         return False
@@ -28,5 +28,14 @@ def do_deploy(archive_path):
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run(f"ln -s {Path}{fileNo} /data/web_static/current").failed is True:
+    if run(f"ln -s {Path}{fileNo}/ /data/web_static/current").failed is True:
         return False
+
+# [52.55.249.213] put: versions/web_static_20170315003959.tgz -> /tmp/web_static_20170315003959.tgz
+# [52.55.249.213] run: mkdir -p /data/web_static/releases/web_static_20170315003959/
+# [52.55.249.213] run: tar -xzf /tmp/web_static_20170315003959.tgz -C /data/web_static/releases/web_static_20170315003959/
+# [52.55.249.213] run: rm /tmp/web_static_20170315003959.tgz
+# [52.55.249.213] run: mv /data/web_static/releases/web_static_20170315003959/web_static/* /data/web_static/releases/web_static_20170315003959/
+# [52.55.249.213] run: rm -rf /data/web_static/releases/web_static_20170315003959/web_static
+# [52.55.249.213] run: rm -rf /data/web_static/current
+# [52.55.249.213] run: ln -s /data/web_static/releases/web_static_20170315003959/ /data/web_static/current
