a1a96bc38cd547ce71d89c692a7808b458611403
ameralnas <amer.live477@gmail.com>
2024-02-18 18:34:32 +0200

    Another console

--- models/city.py
+++ models/city.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-'''
-    Define the class City.
-'''
-from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey
+""" City Module for HBNB project """
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-import models
-from models.state import State
-
 
 class City(BaseModel, Base):
-    '''
-        Define the class City that inherits from BaseModel.
-    '''
-    __tablename__ = "cities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete, delete-orphan")
-    else:
-        state_id = ""
-        name = ""
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
+# hbnb_dev_pwd
--- models/review.py
+++ models/review.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-'''
-    Implementation of the Review class
-'''
-from os import getenv
-from sqlalchemy import Column, String, ForeignKey
+""" Review module for the HBNB project """
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-
 
 class Review(BaseModel, Base):
-    '''
-        Implementation for the Review.
-    '''
-    __tablename__ = "reviews"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id") ,nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
