4546558e07c9467f9347e458b09ca1775c13760c
ameralnas <amer.live477@gmail.com>
2024-02-18 03:22:34 +0200

    What' going on

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Float, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from os import getenv
+from models.city import City
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
