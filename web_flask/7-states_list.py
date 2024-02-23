483ca307ed9b1140dbb1d3eb06487f4c774103b7
ameralnas <amer.live477@gmail.com>
2024-02-23 11:07:54 +0200

    Bored from this

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_method(exception):
+    """Closing the database"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def list_static(states):
+    """A route here"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
+    """Starting the app"""
     app.run(host='0.0.0.0')
